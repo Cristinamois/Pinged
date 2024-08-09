7a17523051d1be5c81f733c6b80a47169a3b5e61
Lucifer <moiscristina@gmail.com>
2024-08-09 16:10:34 +0200

    changed animation for home and login when hover

--- earthping/src/component/Header/Header.js
+++ earthping/src/component/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
           <li><a href="/">Home</a></li>
           {/* <li><a href="/about">About</a></li> */}
           {/* <li><a href="/contact">Contact</a></li> */}
-          <li><a id='login' href="/connnection">Login</a></li>
+          <li><a className="login" href="/connnection">Login</a></li>
         </ul>
       </nav>
     </header>
