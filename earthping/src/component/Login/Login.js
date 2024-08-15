d1ef8e19dfd2fdf5d2b1716176de53cb528c80c3
Lucifer <moiscristina@gmail.com>
2024-08-15 15:56:41 +0200

    login signup done

--- earthping/src/component/Login/Login.js
+++ earthping/src/component/Login/Login.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Login.css';
 import earthDraw from '../../assets/images/cE.png'
 import LoginForm from '../LoginForm/LoginForm';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
     <div className='signup-container-all'>
       <div className='lgBox'>
         <div className='leftGrid'>
+        <Link to="/" className='link'>&lt;- Back</Link>
+          {/* <p>&lt;- Back</p> */}
           <img src={earthDraw} alt='Drawing of planet earth' />
         {/* <p>Planet earth draw</p> */}
         </div>
--- earthping/src/component/Signup/Signup.js
+++ earthping/src/component/Signup/Signup.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Signup.css'
 import earthDraw from '../../assets/images/cE.png'
 import SignupForm from '../SignupForm/SignupForm';
+import { Link } from 'react-router-dom';
 
 function Signup() {
   return (
     <div className='signup-container-all'>
       <div className='lgBox'>
         <div className='leftGrid'>
+        <Link to="/" className='link'>&lt;- Back</Link>
           <img src={earthDraw} alt='Drawing of planet earth' />
         {/* <p>Planet earth draw</p> */}
         </div>
