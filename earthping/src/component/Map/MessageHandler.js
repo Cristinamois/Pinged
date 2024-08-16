f43f9239b3d3717b6a0ffff09024950788107e85
Lucifer <moiscristina@gmail.com>
2024-08-16 11:57:53 +0200

    message precedent s efface bien maintenant

--- earthping/src/component/Map/MessageHandler.js
+++ earthping/src/component/Map/MessageHandler.js
@@ -1,6 +1,14 @@
 import L from 'leaflet';
 
-export function displayMessage(map, position, message, duration = 10 * 1000) { 
+// Variable pour stocker le popup actuellement ouvert
+let currentPopup = null;
+
+export function displayMessage(map, position, message, duration = 10 * 1000) {
+  // Fermer l'ancien popup s'il existe
+  if (currentPopup) {
+    map.removeLayer(currentPopup);
+  }
+
   const popupContent = `
     <div id="popup-content">
       <p>${message}</p>
@@ -11,18 +19,29 @@ export function displayMessage(map, position, message, duration = 10 * 1000) {
     closeOnClick: false,
     autoClose: false,
     closeButton: false,
+    className: 'custom-popup' // Ajouter la classe personnalisée ici
   })
     .setLatLng(position)
     .setContent(popupContent)
     .openOn(map);
 
+  // Mettre à jour la référence au popup actuel
+  currentPopup = popup;
+
   setTimeout(() => {
-    map.removeLayer(popup);
-    createPopup(map, position); // Réaffiche le formulaire après l'expiration du message
+    if (currentPopup === popup) { // Vérifier si c'est le même popup qui est encore ouvert
+      map.removeLayer(popup);
+      createPopup(map, position); // Réaffiche le formulaire après l'expiration du message
+    }
   }, duration);
 }
 
 export function createPopup(map, position) {
+  // Fermer l'ancien popup s'il existe
+  if (currentPopup) {
+    map.removeLayer(currentPopup);
+  }
+
   const popupContent = `
     <div id="popup-content">
       <form id="message-form">
@@ -32,12 +51,18 @@ export function createPopup(map, position) {
     </div>
   `;
 
-  const popup = L.popup({ closeOnClick: false, autoClose: false })
+  const popup = L.popup({
+    closeOnClick: false,
+    autoClose: false,
+    className: 'custom-popup' // Ajouter la classe personnalisée ici
+  })
     .setLatLng(position)
     .setContent(popupContent)
     .openOn(map);
 
-  // Ajouter un écouteur d'événements pour gérer la soumission du formulaire
+  // Mettre à jour la référence au popup actuel
+  currentPopup = popup;
+
   document.getElementById('message-form').addEventListener('submit', function(e) {
     e.preventDefault();
     const message = document.getElementById('message-input').value;
